64849bfaea17a74dcd940a043b1ceb47f3d14607
Paul Betts <paul@paulbetts.org>
2012-07-07 21:06:07 -0700

    Make the WhereAmI app look up location and print to console

---
+++ Whereami/Whereami/WhereamiViewController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.CoreLocation;
+using MonoTouch.UIKit;
+
+namespace Whereami
+{
+    public partial class WhereamiViewController : UIViewController
+    {
+        CLLocationManager locationManager;
+    
+        public WhereamiViewController () : base ("WhereamiViewController", null)
+        {
+        }
+        
+        public override void DidReceiveMemoryWarning ()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning ();
+            
+            // Release any cached data, images, etc that aren't in use.
+        }
+        
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            
+            locationManager = new CLLocationManager();
+            locationManager.DesiredAccuracy = 0.2;
+            locationManager.StartUpdatingLocation();
+            locationManager.Delegate = new WhereAmILocationDelegate();
+        }
+        
+        public override void ViewDidUnload ()
+        {
+            base.ViewDidUnload ();
+            
+            // Clear any references to subviews of the main view in order to
+            // allow the Garbage Collector to collect them sooner.
+            //
+            // e.g. myOutlet.Dispose (); myOutlet = null;
+            
+            ReleaseDesignerOutlets ();
+        }
+        
+        public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+        {
+            // Return true for supported orientations
+            return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+        }
+    }
+    
+    public class WhereAmILocationDelegate : CLLocationManagerDelegate
+    {
+        public override void UpdatedLocation (CLLocationManager manager, CLLocation newLocation, CLLocation oldLocation)
+        {
+            Console.WriteLine("{0},{1}", newLocation.Coordinate.Latitude, newLocation.Coordinate.Longitude);
+        }
+        
+        public override void Failed (CLLocationManager manager, NSError error)
+        {
+            Console.WriteLine("Could not find location: {0}", error);
+        }
+    }
+}
